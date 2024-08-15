91b30d185c953d7e40df936e6dd4d693d60d593a
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-15 13:28:36 -0400

    Initial Benifitial

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Assignment {
+public class Assignment {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+
+
+    }
+
+     public static void printMegaBytesAndKiloBytes(int kilobytes){
+         if (kilobytes >= 0) {
+             int mega = kilobytes / 1024;
+             int kb = kilobytes % 1024;
+             System.out.println(kilobytes + " KB " + mega + " MB and " + kb + " KB");
+         } else {
+             System.out.println("Invalid Value");
+
+
+         }
+
+
+     }
+
 }
